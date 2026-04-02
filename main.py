9ccb182f61507ab5d77323278895f79fd416170c
Tarık <67359075+Tarik366@users.noreply.github.com>
2026-04-02 12:37:42 +0300

    Updated for Wintoys

--- main.py
+++ main.py
@@ -4,15 +4,10 @@ import re
 
 rte = rotatescreen.get_primary_display()
 
-def change_rotation():
-    cr = rte.current_orientation
-    if cr == 0:
-        rte.set_portrait()
-    else:
-        rte.set_landscape()
+cr = rte.current_orientation
+if cr == 0:
+    rte.set_portrait()
+else:
+    rte.set_landscape()
 
-import keyboard  # using module keyboard
-
-keyboard.add_hotkey('win + page up', change_rotation)  # create hotkey
-keyboard.wait('')
     
